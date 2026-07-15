ae799427831d15e228998a08786dda648a08a6c0
Abdul Ghaffar <aghaffar570@gmail.com>
2026-07-15 14:31:25 -0400

    add food fetch call

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 
 function App() {
   const [users, setUsers] = useState([])
-  const BASE_API_URL = import.meta.env.VITE_API_URL
+  const [foods, setFoods] = useState([])
+  const BASE_API_URL = import.meta.env.VITE_API_URL || 'https://deploy-backend-demo.onrender.com'
 
   useEffect(() => {
     fetch(BASE_API_URL + '/users')
@@ -11,6 +12,12 @@ function App() {
       .then(data => {
         console.log(data)
         setUsers(data)
+      })
+    fetch(BASE_API_URL + '/foods')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setFoods(data)
       })
   }, [])
 
@@ -21,6 +28,11 @@ function App() {
       {
         users.map((u) => (
           <p key={u.id}>User: {u.name}</p>
+        ))
+      }
+      {
+        foods.map((f) => (
+          <p key={f.id}>Food: {f.name}</p>
         ))
       }
     </div>
